be55997eeee72011e9963226553d4908f78c29b9
Exveanor <eviexadore@gmail.com>
2023-12-07 00:02:52 +0200

    Update 02vacation.js

--- FrontEndDev/Syntax/02vacation.js
+++ FrontEndDev/Syntax/02vacation.js
@@ -8,13 +8,25 @@ function vacation (groupOfPeople, type, day) {
         } else if (day === "Sunday") {
             price = groupOfPeople * 10.46;
         }
+
+        if (groupOfPeople >= 30) {
+            price *= 0.85;
+        }
     } else if (type === "Business") {
+        let pricePerTicket = 0;
         if(day === "Friday") {
             price = groupOfPeople * 10.90;
+            pricePerTicket = 10.90;
         } else if (day === "Saturday") {
             price = groupOfPeople * 15.60;
+            pricePerTicket = 15.60;
         } else if (day === "Sunday") {
             price = groupOfPeople * 16.00;
+            pricePerTicket = 16.00;
+        }
+
+        if (groupOfPeople >= 100) {
+            price -= pricePerTicket * 10;
         }
     } else {
         if(day === "Friday") {
@@ -24,5 +36,13 @@ function vacation (groupOfPeople, type, day) {
         } else if (day === "Sunday") {
             price = groupOfPeople * 22.50;
         }
+
+        if (groupOfPeople >= 10 && groupOfPeople <= 20) {
+            price *= 0.95;
+        }
     }
+
+    console.log (`Total price: ${price.toFixed(2)}`);
 }
+
+vacation(30, "Students", "Sunday")
