084c506847584480e69719c34ee28a07c98967dc
Exveanor <eviexadore@gmail.com>
2024-03-06 23:42:20 +0200

    Update arrayRotation.js

--- FrontEndDev/arraysAndStrings/arrayRotation.js
+++ FrontEndDev/arraysAndStrings/arrayRotation.js
@@ -1,5 +1,13 @@
 function arrRotation (givenArr, rotations) {
 
+    for (let i = 0; i < rotations; i++) {
+        const element = givenArr.shift();
+        givenArr.push(element);
+        
+    }
+
+    console.log(givenArr.join(" "));
+
 }
 
-arrRotation ([51, 47, 32, 61, 21], 2)
+arrRotation ([51, 47, 32, 61, 21], 2);
