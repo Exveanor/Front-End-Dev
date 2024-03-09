3a9880ecca817f962f9e4f303f0825be0c4e4a77
Exveanor <eviexadore@gmail.com>
2024-03-09 20:32:09 +0200

    Update printEveryNthElement.js

--- FrontEndDev/arraysAndStrings/printEveryNthElement.js
+++ FrontEndDev/arraysAndStrings/printEveryNthElement.js
@@ -1,5 +1,14 @@
 function print (givenArr, step) { 
 
+    let output = [];
+    let arrayLength = givenArr.length;
+
+    for (let index = 0; index < arrayLength ; index+=step) {
+        let currentNum = givenArr[index];
+        output.push(currentNum);
+    }
+
+    return output;
 }
 
 print (['5','20','31','4','20'], 2);
