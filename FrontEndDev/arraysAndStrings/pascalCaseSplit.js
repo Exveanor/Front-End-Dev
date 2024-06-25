47890e9a814dbd61837653f0c885b7559f6ae3d8
Exveanor <eviexadore@gmail.com>
2024-06-25 20:57:38 +0300

    Update pascalCaseSplit.js

--- FrontEndDev/arraysAndStrings/pascalCaseSplit.js
+++ FrontEndDev/arraysAndStrings/pascalCaseSplit.js
@@ -1,6 +1,10 @@
 function pascalCaseSplit (text) {
 
-    
+    let wordsRegex = /[A-Z][a-z]*/g;
+    let wordsArray = text.match(wordsRegex);
+    let resultString = wordsArray.join(", ");
+
+    console.log(resultString);
 
 }
 
