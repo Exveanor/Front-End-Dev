9e4be29e297eef4baa4dffd9372c5f206e070cee
Exveanor <eviexadore@gmail.com>
2024-07-21 02:51:59 +0300

    Update matrix.js

--- FrontEndDev/functions/matrix.js
+++ FrontEndDev/functions/matrix.js
@@ -1,5 +1,13 @@
 function simpleMatrix (number) {
 
+    function printRow(num) {
+        let singleRow = (num.toString() + " ").repeat(num);
+        return singleRow;
+    }
+
+    for(let currentRow = 1; currentRow <= number; currentRow++) {
+        console.log(printRow(number));
+    }
 
 }
 
