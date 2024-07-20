d85379fa03aff0119255a891d282c6ca68999575
Exveanor <eviexadore@gmail.com>
2024-07-21 02:37:40 +0300

    Update passwordValidator.js

--- FrontEndDev/functions/passwordValidator.js
+++ FrontEndDev/functions/passwordValidator.js
@@ -1,54 +1,46 @@
-function passwordValidator (password) {
+function passwordValidator(password) {
+  function isValidLength(text) {
+    return text.length >= 6 && text.length <= 10;
+  }
 
-    function isValidLength (text) {
-        
-        return text.length >= 6 && text.length <= 10;
+  function isAlphanumeric(text) {
+    let regex = /^[a-zA-Z0-9]+$/;
+    let isCorrect = regex.test(text);
+
+    return isCorrect;
+  }
+
+  function checkDigits(text) {
+    let digitsCounter = 0;
+
+    for (const digit of text) {
+      if (!isNaN(digit)) {
+        digitsCounter++;
+      }
     }
 
-    function isAlphanumeric(text) {
+    return digitsCounter >= 2;
+  }
 
-        let regex = /^[a-zA-Z0-9]+$/;
-        let isCorrect = regex.test(text);
+  let validLength = isValidLength(password);
+  let validNumeric = isAlphanumeric(password);
+  let validDigitsCount = checkDigits(password);
 
-        return isCorrect;
-    }
+  if (!validLength) {
+    console.log("Password must be between 6 and 10 characters");
+  }
 
-    function checkDigits(text) {
+  if (!validNumeric) {
+    console.log("Password must consist only of letters and digits");
+  }
 
-        let digitsCounter = 0;
+  if (!validDigitsCount) {
+    console.log("Password must have at least 2 digits");
+  }
 
-        for (const digit of text) {
-            if(!isNaN(digit)) {
-                digitsCounter++;
-            }
-        }
-
-        return digitsCounter >= 2;
-    }
-
-    let validLength = isValidLength(password);
-    let validNumeric = isAlphanumeric(password);
-    let validDigitsCount = checkDigits(password);
-
-
-    if(!validLength) {
-        console.log("Password must be between 6 and 10 characters");
-    }
-
-    if(!validNumeric) {
-        console.log("Password must consist only of letters and digits");
-    }
-
-    if(!validDigitsCount) {
-        console.log("Password must have at least 2 digits");
-    }
-
-    if(validLength && validNumeric && validDigitsCount) {
-
-        console.log("Password is valid");
-
-    }
-
+  if (validLength && validNumeric && validDigitsCount) {
+    console.log("Password is valid");
+  }
 }
 
-passwordValidator ("Mypass123");
+passwordValidator("Mypass123");
